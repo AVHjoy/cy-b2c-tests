109dba2416d437ab58f4e2bd21504072bd823163
Joyce Oliveira <joymoliveirag@gmail.com>
2023-12-04 15:54:29 -0300

    Update Login.cy.js
    preencher login com usuario especifico

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -7,9 +7,46 @@ class Login {
         cy.visit('https://b2clubwebhomolog.azurewebsites.net/login')
     }
 
-    preencherLogin() {
-    cy.get(element.user).click().type('vinijr')
-    cy.get(element.password).type('123456')
+    preencherLogin(tipoUsuario) {
+        switch(tipoUsuario) {
+            case 'InfluencerBR':
+                cy.get(element.user).click().type('vinijr')
+                cy.get(element.password).type('123456')
+                break;
+            case 'GestorBR':
+                cy.get(element.user).click().type('####')
+                cy.get(element.password).type('123456')
+                break;
+            case 'AdminBR':
+                cy.get(element.user).click().type('admin')
+                cy.get(element.password).type('123456')
+                break;    
+            case 'InfluencerUSA':
+                cy.get(element.user).click().type('eduteste')
+                cy.get(element.password).type('123456')
+                break;
+            case 'GestorUSA':
+                cy.get(element.user).click().type('keila')
+                cy.get(element.password).type('123456')
+                break;
+            case 'AdminUSA':
+                cy.get(element.user).click().type('#####')
+                cy.get(element.password).type('123456')
+                break;
+            case 'InfluencerFRA':
+                cy.get(element.user).click().type('####')
+                cy.get(element.password).type('123456')
+                break;
+            case 'GestorFRA':
+                cy.get(element.user).click().type('equpeFrancaA2')
+                cy.get(element.password).type('123456')
+                break;
+            case 'AdminFRA':
+                cy.get(element.user).click().type('b2cfrance')
+                cy.get(element.password).type('123456')
+                break;
+        }
+
     cy.get(element.buttonEntrar).click()
 
     }
